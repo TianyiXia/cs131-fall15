a4bda8b4f8eb24abdbb22e6896ec9282ef249d29
qingweilan <qingweilan@gmail.com>
2015-09-29 16:46:22 -0700

    Added set_union

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -9,3 +9,14 @@ let rec subset a b =
 (* equal_sets: returns true iff the represented sets are equal *)
 let rec equal_sets a b = subset a b && subset b a;;
 
+(* set_union: returns a list representing the union of two sets *)
+let rec set_union a b = 
+	match a, b with
+	| [], [] -> []
+	| h1::t1, [] -> set_union [h1] t1
+	| [], h2::t2 -> set_union [h2] t2
+	| h1::t1, h2::t2 -> 
+	if not (subset [h1] t1) && not (subset [h1] t2) && not (subset [h2] t1) && not (subset [h2] t2) then (set_union t1 t2) @ h1 @ h2 
+	else if not (subset [h1] t1) && not (subset [h1] t2) then (set_union t1 t2) @ h1
+	else if not (subset [h2] t1) && not (subset [h2] t2) then (set_union t1 t2) @ h2
+	else set_union t1 t2;;
