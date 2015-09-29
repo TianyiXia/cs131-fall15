e04723d8f104df68f6ce6aa5ff1e5b1f59e1fa40
qingweilan <qingweilan@gmail.com>
2015-09-29 16:00:30 -0700

    Added equal_sets

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -4,6 +4,8 @@ let rec subset a b =
 	| [], _ -> true
 	| _, [] -> false
 	| [a], [b] -> if a == b then true else false
-	| h1::t1, h2::t2 -> 
-	if (if h1 == h2 then true else subset [h1] t2) then subset t1 b else false;;
+	| h1::t1, h2::t2 -> if (if h1 == h2 then true else subset [h1] t2) then subset t1 b else false;;
+
+(* equal_sets: returns true iff the represented sets are equal *)
+let rec equal_sets a b = subset a b && subset b a;;
 
