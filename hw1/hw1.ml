065e1a44a7c87b55e23d5747d9032200071ad549
qingweilan <qingweilan@gmail.com>
2015-09-29 15:33:03 -0700

    Added function subset

---
+++ hw1/hw1.ml
@@ -0,0 +1,9 @@
+(* subset: returns true iff a is subset of b *)
+let rec subset a b =
+	match a, b with
+	| [], _ -> true
+	| _, [] -> false
+	| [a], [b] -> if a == b then true else false
+	| h1::t1, h2::t2 -> 
+	if (h1 == h2 then true else subset [h1] t2) then subset t1 b else false;;
+
