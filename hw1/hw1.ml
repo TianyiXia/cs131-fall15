da2090f374185b3f9f1fdb00dd3da77fac5c7af0
qingweilan <qingweilan@gmail.com>
2015-09-29 20:31:06 -0700

    Changed log of set_intersection to use set_union

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -7,7 +7,7 @@ let rec subset a b =
 	| h1::t1, h2::t2 -> if (if h1 = h2 then true else subset [h1] t2) then subset t1 b else false;;
 
 (* equal_sets: returns true iff the sets are equal *)
-let rec equal_sets a b = subset a b && subset b a;;
+let equal_sets a b = subset a b && subset b a;;
 
 (* set_union: returns a list of the union of two sets *)
 let rec set_union a b = 
@@ -29,9 +29,11 @@ let rec set_intersection a b =
 	match a, b with
 	| _, [] -> []
 	| [], _ -> []
-	| h1::t1, h2::t2 -> 
-	if h1 = h2 then (set_intersection t1 t1) @ [h1]
+	| h1::t1, b ->
+	if subset [h1] b then set_union (set_intersection t1 b) [h1] else set_intersection t1 b;;
+
+(* 	if h1 = h2 then (set_intersection t1 t1) @ [h1]
 	else if subset [h1] t2 && subset [h2] t1 then (set_intersection t1 t2) @ [h1] @ [h2]
 	else if subset [h1] t2 then (set_intersection t1 t2) @ [h1] 
 	else if subset [h2] t1 then (set_intersection t1 t2) @ [h2]
-	else set_intersection t1 t2;; 
+	else set_intersection t1 t2;; *)
