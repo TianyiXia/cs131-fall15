bdec838020d79b28e686a76b8d3ef119fa10b83c
qingweilan <qingweilan@gmail.com>
2015-09-29 17:02:50 -0700

    Fixed typo for type

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -14,9 +14,9 @@ let rec set_union a b =
 	match a, b with
 	| [], [] -> []
 	| h1::t1, [] -> 
-	if subset [h1] t1 then set_union [] t1 else (set_union [] t1) @ h1
+	if subset [h1] t1 then set_union [] t1 else (set_union [] t1) @ [h1]
 	| [], h2::t2 ->
-	if subset [h2] t2 then set_union [] t2 else (set_union [] t2) @ h2
+	if subset [h2] t2 then set_union [] t2 else (set_union [] t2) @ [h2]
 	| h1::t1, h2::t2 -> 
 	if not (subset [h1] t1) && not (subset [h1] t2) && not (subset [h2] t1) && not (subset [h2] t2) then (set_union t1 t2) @ [h1] @ [h2] 
 	else if not (subset [h1] t1) && not (subset [h1] t2) then (set_union t1 t2) @ [h1]
