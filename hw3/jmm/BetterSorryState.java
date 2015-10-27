6867330fa3bdcca5ed8a1f4e9672500e331453d6
QINGWEI LAN <qingweilan@gmail.com>
2015-10-26 22:30:34 -0700

    Modified BetterSorry

--- hw3/jmm/BetterSorryState.java
+++ hw3/jmm/BetterSorryState.java
@@ -1,41 +1,25 @@
 import java.util.concurrent.atomic.AtomicInteger;
 
 class BetterSorryState implements State {
-	private AtomicInteger[] value;
+	private byte[] value;
 	private byte maxval;
 
-	BetterSorryState(byte[] v) {
-		value = new AtomicInteger[v.length];
-		for (int i =0; i < v.length; i++) {
-			value[i] = new AtomicInteger(v[i]);
-		}
-		maxval = 127;
-	}
+	BetterSorryState(byte[] v) { value = v; maxval = 127; }
 
-	BetterSorryState(byte[] v, byte m) {
-		value = new AtomicInteger[v.length];
-		for (int i =0; i < v.length; i++) {
-			value[i] = new AtomicInteger(v[i]);
-		}
-		maxval = m;
-	}
+	BetterSorryState(byte[] v, byte m) { value = v; maxval = m; }
 
 	public int size() { return value.length; }
 
-	public byte[] current() {
-		byte[] current = new byte[value.length];
-		for (int i = 0; i < value.length; i++) {
-			current[i] = (byte) value[i].get();
-		}
-		return current;
-	}
+	public byte[] current() { return value; }
 
 	public boolean swap(int i, int j) {
-		if (value[i].get() <= 0 || value[j].get() >= maxval) {
+		if (value[i] <= 0 || value[j] >= maxval) {
 			return false;
 		}
-		value[i].getAndDecrement();
-		value[j].getAndIncrement();
+		AtomicInteger aii = new AtomicInteger(value[i]);
+		AtomicInteger aij = new AtomicInteger(value[j]);
+		value[i] = (byte) aii.getAndDecrement();
+		value[j] = (byte) aij.getAndIncrement();
 		return true;
 	} 
 }
