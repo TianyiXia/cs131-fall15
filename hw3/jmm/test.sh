1571b946d83a83cf245df0b737702e7c4fcebc43
QINGWEI LAN <qingweilan@gmail.com>
2015-10-26 22:18:51 -0700

    Lowered number of tests per trial

--- hw3/jmm/test.sh
+++ hw3/jmm/test.sh
@@ -7,7 +7,7 @@ run="java"
 
 echo "  * Null Test *"
 echo ""
-for i in {1..50}
+for i in {1..20}
 do
 	# echo -en "  $i\t"
 	$run UnsafeMemory Null $1 $2 100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
@@ -16,7 +16,7 @@ echo ""
 
 echo "  * Synchronized Test *"
 echo ""
-for i in {1..50}
+for i in {1..20}
 do
 	# echo -en "  $i\t"
 	$run UnsafeMemory Synchronized $1 $2 100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
@@ -25,16 +25,16 @@ echo ""
 
 echo "  * Unsynchronized Test"
 echo ""
-for i in {1..50}
+for i in {1..20}
 do
 	# echo -en "  $i\t"
-	$run UnsafeMemory Unsynchronized $1 $2 20 5 6 3 0 3100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
+	$run UnsafeMemory Unsynchronized $1 $2 100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
 done
 echo ""
 
 echo "  * GetNSet Test *"
 echo ""
-for i in {1..50}
+for i in {1..20}
 do
 	# echo -en "  $i\t"
 	$run UnsafeMemory GetNSet $1 $2 100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
@@ -43,7 +43,7 @@ echo ""
 
 echo "  * BetterSafe Test *"
 echo ""
-for i in {1..50}
+for i in {1..20}
 do
 	# echo -en "  $i\t"
 	$run UnsafeMemory BetterSafe $1 $2 100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
@@ -52,7 +52,7 @@ echo ""
 
 echo "  * BetterSorry Test *"
 echo ""
-for i in {1..50}
+for i in {1..20}
 do
 	# echo -en "  $i\t"
 	$run UnsafeMemory BetterSorry $1 $2 100 2 4 6 8 10 10 20 30 40 50 5 6 3 0 3 10 20 30 40 50 | grep -Eow "[0-9.]+"
