ba02c0e70683e81f38fc26bb7b82a6cfb85d3480
qingweilan <qingweilan@gmail.com>
2015-11-30 23:41:29 -0800

    Fixed test.sh

--- project/test.sh
+++ project/test.sh
@@ -8,14 +8,12 @@ sleep 2
 	sleep 1
 	echo WHATSAT qingwei.seas.ucla.edu 10 6
 	sleep 2
-	echo quit
 } | telnet lnxsrv.seas.ucla.edu 12551 # Bolden
 
 {
 	sleep 2
 	echo IAMAT anqi.seas.ucla.edu +35.068930-118.445127 1400994645.392014450
 	sleep 1
-	echo quit
 } | telnet lnxsrv.seas.ucla.edu 12550 # Alford
 
 # kill Alford
@@ -27,7 +25,6 @@ pkill -f 'python chat.py Alford'
 	sleep 1
 	echo WHATSAT qingwei.seas.ucla.edu 20 5
 	sleep 2
-	echo quit
 } | telnet lnxsrv.seas.ucla.edu 12554
 
 # kill all servers
