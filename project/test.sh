92ece3effd86df4c0fae1f439e3b92ecb8ca6592
qingweilan <qingweilan@gmail.com>
2015-11-30 23:45:31 -0800

    Finalized tests

--- project/test.sh
+++ project/test.sh
@@ -25,7 +25,29 @@ pkill -f 'python chat.py Alford'
 	sleep 1
 	echo WHATSAT qingwei.seas.ucla.edu 20 5
 	sleep 2
-} | telnet lnxsrv.seas.ucla.edu 12554
+} | telnet lnxsrv.seas.ucla.edu 12554 # Powell
+
+# startup Alford
+python chat.py Alford
+
+{
+	sleep 1
+	echo IAMAT qingweipeterlan.seas.ucla.edu +31.068930-118.445127 1400994645.392014450
+	sleep 1
+	echo WHATSAT qingwei.seas.ucla.edu 20 5
+	sleep 2
+} | telnet lnxsrv.seas.ucla.edu 12552 # Hamilton
+
+# kill Hamilton
+pkill -f 'python chat.py Hamilton'
+
+{
+	sleep 1
+	echo WHATSAT qingweipeterlan.seas.ucla.edu 20 5
+	sleep 1
+	echo WHATSAT qingwei.seas.ucla.edu 20 5
+	sleep1
+} | telnet lnxsrv.seas.ucla.edu 12550 # Alford
 
 # kill all servers
 ./kill.sh
